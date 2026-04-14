27e7d10020bc1dafeca0dd93a9564b13fa29859a
Sam Johnson <sambjohnson94@gmail.com>
2026-04-14 21:14:14 +0200

    fix source formatting and url

--- user/dasm/template.py
+++ user/dasm/template.py
@@ -4,6 +4,8 @@ pkgrel = 0
 build_style = "makefile"
 pkgdesc = "Macro assembler with support for several 8-bit microprocessors"
 license = "GPL-2.0-only"
-url = "https://dasm-assembler.github.io/"
-source = f"https://github.com/dasm-assembler/dasm/archive/refs/tags/{pkgver}.tar.gz"
+url = "https://dasm-assembler.github.io"
+source = (
+    f"https://github.com/dasm-assembler/dasm/archive/refs/tags/{pkgver}.tar.gz"
+)
 sha256 = "ec71ffd10eeaa70bf7587ee0d79a92cd3f0a017c0d6d793e37d10359ceea663a"
